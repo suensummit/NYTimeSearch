9a087be23b2de40296cd3a1d62ceafbf0e8ff8fa
Summit Suen <summit.suen@gmail.com>
2017-02-26 02:59:32 +0800

    [DEV] video 26:55.

--- app/src/main/java/com/example/suensummit/nytimesearch/SearchActivity.java
+++ app/src/main/java/com/example/suensummit/nytimesearch/SearchActivity.java
@@ -1,15 +1,31 @@
 package com.example.suensummit.nytimesearch;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    Button btnSearch;
+    GridView gvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +33,15 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupViews();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
+
+    public void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
     }
 
     @Override
@@ -48,5 +64,36 @@ public class SearchActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+
+        // Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String API_KEY = "ff05a054b564490d96720a8b78904d5f";
+        String URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key", API_KEY);
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+
+                JSONArray articleResults = null;
+
+                try {
+                    articleResults = response.getJSONObject("response").getJSONArray("docs");
+                    Log.d("DEBUG", articleResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
